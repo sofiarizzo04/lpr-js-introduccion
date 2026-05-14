50172138952a1a914bf800fd08f0747559895614
sofiarizzo04 <sofirizzo412@gmail.com>
2026-05-14 19:12:20 -0300

    resto

--- homework1.js
+++ homework1.js
@@ -135,16 +135,15 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+ return num % 2 === 0; 
  
-
-
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-
+ 
   
 
 }
