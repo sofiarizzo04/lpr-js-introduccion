89ac16ed521ef238d2376a1dde4e12c00057559d
sofiarizzo04 <sofirizzo412@gmail.com>
2026-05-15 16:20:09 -0300

    elevaralcubo

--- homework1.js
+++ homework1.js
@@ -193,7 +193,9 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
- 
+ function str () {
+  console.log (str!);  
+ }
   
 
 
@@ -203,16 +205,18 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Juan " -> "Soy Juan"
   // Tu código:
+   return (sofia + "" + rizzo)
 
-  
 
 }
+
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
- 
+  return ("hola" + sofia + "!");
+
   
  
   
