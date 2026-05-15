956ccc9c8878ba2bf96a283e442a04c340db7c7d
sofiarizzo04 <sofirizzo412@gmail.com>
2026-05-15 16:46:19 -0300

    redondearnumero

--- homework1.js
+++ homework1.js
@@ -195,10 +195,7 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
- function str () {
-
- }
-  
+ return str + "!";  
 
 
 }
@@ -207,6 +204,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Juan " -> "Soy Juan"
   // Tu código:
+return nombre + " " + apellido;
 
 
 }
@@ -216,6 +214,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  return ("hola" + " " + nombre + "!");
   
 
   
